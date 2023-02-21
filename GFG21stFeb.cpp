e5f39f322a6f8add1fc32f5fc115e9d5ee433481
webdforall <67256853+webdforall@users.noreply.github.com>
2023-02-21 23:05:23 +0530

    Update GFG21stFeb.cpp

--- GFG21stFeb.cpp
+++ GFG21stFeb.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 class Solution{   
 public:
+	/* This code is also better and easy to understand and second one is also easy
+	
+	int minIteration(int N, int M, int x, int y){    
+	     int ans=0;
+	     for(int i=0;i<=N;i++){
+		 for(int j=0;j<=M;j++){
+		     ans=max(ans,abs(i-x)+abs(j-y));
+		 }
+	     }
+	     return ans;
+	    }
+	*/
     int minIteration(int N, int M, int x, int y){    
         // code here
        int top = x-1, down = N-x;
