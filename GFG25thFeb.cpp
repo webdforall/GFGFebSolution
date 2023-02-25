0778358b4f0b51ab1ab80b72916096af726db909
Kuldeep <kuldeepissingh36@gmail.com>
2023-02-25 15:00:09 +0530

    25thFebSol

---
+++ GFG25thFeb.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int checkCompressed(string S, string T) {
+        // code here
+        int i = 0; 
+        int j = 0;
+        int n = S.size();
+        int m = T.size();
+        while(i<n && j<m){
+            if(isdigit(T[j])){
+                int count = 0;
+                while (j <m  && isdigit(T[j])) {
+                   count = count * 10 + (T[j] - '0');
+                     j++;
+                }
+                 i += count;
+            }
+            else if(S[i] == T[j]){
+                i++;
+                j++;
+            }else if(S[i] != T[j]) return 0;
+        }
+        return i == n && j == m;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S,T;
+        
+        cin>>S>>T;
+
+        Solution ob;
+        cout << ob.checkCompressed(S,T) << endl;
+    }
+    return 0;
+}
