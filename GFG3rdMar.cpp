aae8f0b1fdcc03f434db9ee7fc0f784765741da2
Kuldeep <kuldeepissingh36@gmail.com>
2023-03-03 21:20:40 +0530

    3rdMarSol

---
+++ GFG3rdMar.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        // code here
+        long long int a = __gcd(L,B);
+        long long int b = (L*B)/(a*a);
+        return {b,a};
+    }
+};
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int L, B;
+        cin>>L>>B;
+        
+        Solution ob;
+        vector<long long int> ans = ob.minimumSquares(L, B);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}
